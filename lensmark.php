24fb166b2985cfef05ebbe9692819a53fba77779
Martin Clément <martin.clement@outlook.com>
2023-02-22 17:19:14 +0100

    initialize git-repo and lensmark.php file

---
+++ lensmark.php
@@ -0,0 +1,22 @@
+/**
+ * LENSMARK
+ *
+ * @package           LensmarkPackage
+ * @author            Martin Clément
+ * @copyright         2023 Berner Fachhochschule
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       LENSMARK
+ * Plugin URI:        https://wbth.m-clement.ch/
+ * Description:       Photomonitoring Plugin for Wordpress
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Martin Clément
+ * Author URI:        https://m-clement.ch/
+ * Text Domain:       landmark
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
