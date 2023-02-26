d81aab4204700e2c1dd788d163cd609297ae510f
Martin Clément <martin.clement@outlook.com>
2023-02-26 18:35:52 +0100

    Initial setup of boilerplate

---
+++ includes/class-lensmark-activator.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ * @link       http://wbth.m-clement.ch/
+ * @since      0.1.0
+ * @package    Lensmark
+ * @subpackage Lensmark/includes
+ */
+
+class Lensmark_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * @since    0.1.0
+	 */
+	public static function activate() {
+		
+	}
+
+}
+
