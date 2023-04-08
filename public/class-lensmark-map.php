3596b9b5d7cc6dff38d8817798487ce00f1cb543
Martin Clément <martin.clement@outlook.com>
2023-04-08 18:08:13 +0200

    Restructure public facing functionalities

---
+++ public/class-lensmark-map.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Contains all map functionalities. 
+ * 
+ * [lensmark-map-overview] is a leaflet.js map which displays all photoposts.
+ *
+ * @link       https://wbth.m-clement.ch
+ * @since      1.0.0
+ *
+ * @package    Lensmark
+ * @subpackage Lensmark/public
+ */
+
+class Lensmark_Map {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $lensmark    The ID of this plugin.
+	 */
+	private $lensmark;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $lensmark       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $lensmark, $version ) {
+
+		$this->lensmark = $lensmark;
+		$this->version = $version;
+	}
+
+	/**
+	 * Register the stylesheets
+	 * 
+	 * @since	1.0.0
+	 */
+    public function enqueue_styles() {
+		// enqueue dependencies when the shortcode is on the current page
+		if ( has_shortcode( get_post()->post_content, 'lensmark-map-overview' ) ) {
+		wp_enqueue_style( 'lensmark-map', plugin_dir_url( __FILE__ ) . 'css/lensmark-map.css', array(), $this->version, 'all' );
+		}
+		if ( has_shortcode( get_post()->post_content, 'lensmark-map-overview' ) ) {
+			wp_enqueue_style( 'leaflet-css', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css', array(), '1.9.3', 'all', array( 'integrity' => 'sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=', 'crossorigin' => '' ) );
+		}
+
+	}
+
+	/**
+	 * Register the JavaScript
+	 * 
+	 * @since	1.0.0
+	 */
+    public function enqueue_scripts() {
+		// enqueue dependencies when the shortcode is on the current page
+		if ( has_shortcode( get_post()->post_content, 'lensmark-map-overview' ) ) {
+			wp_enqueue_script( 'leaflet-js', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js', array(), '1.9.3', true, array( 'integrity' => 'sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=', 'crossorigin' => '' ) );
+			wp_enqueue_script( 'lensmark-map', plugin_dir_url( __FILE__ ) . 'js/lensmark-map-overview.js', array( 'jquery', 'leaflet-js' ), $this->version, false );
+			wp_enqueue_script( 'lensmark-ajax', plugin_dir_url( __FILE__ ) . 'js/lensmark-map-overview.js', array( 'jquery' ), $this->version, false );
+			wp_localize_script( 'lensmark-ajax', 'lensmark_ajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ), ) );
+		}
+	}
+
+	/**
+	 * Add the shortcode: [lensmark-map-overview]
+	 * 
+	 * @since	1.0.0
+	 */
+    public function lensmark_add_map_overview_shortcode() {
+        add_shortcode( 'lensmark-map-overview', array($this, 'lensmark_map_overview_callback') );
+    }
+
+	/**
+	 * Callback function of the shortcode [lensmark-map-overview]
+	 * 
+	 * @since	1.0.0
+	 */
+    public function lensmark_map_overview_callback ( $atts, $content = null ) {
+        extract( shortcode_atts( array(
+            'width' => '100%', // default width
+            'height' => '800px', // default height
+        ), $atts ) );
+        ob_start();
+        ?>
+        <div id="map" style="width: <?php echo $width ?>; height: <?php echo $height ?>"></div>
+        <?php
+        return ob_get_clean();
+    }
+
+    /**
+	 * Get photopost including meta-data for javascript usage. Is used to add a pin on the [lensmark-map-overview] map
+	 * for each photopost.
+	 * 
+	 * @since    1.0.0
+	 */
+	public function lensmark_get_photoposts() {
+		// get plugin settings
+		$map_pos_latitude = get_option( 'lensmark_map_latitude',  );
+		$map_pos_longitude = get_option( 'lensmark_map_longitude', );
+		$map_zoom = get_option( 'lensmark_map_zoom');
+	
+		// get photopost specific data
+		$args = array(
+			'post_type' => 'photopost',
+			'posts_per_page' => -1,
+		);
+		$posts = get_posts( $args );
+		$result = array();
+		foreach ( $posts as $post ) {
+			// post specific data
+			$id = $post->ID;
+			$title = $post->post_title;
+			$excerpt = $post->post_excerpt;
+			$latitude = get_post_meta( $id, 'latitude', true );
+			$longitude = get_post_meta( $id, 'longitude', true );
+			$link = get_permalink( $id );
+			$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' );
+			$thumbnail_url = $thumbnail ? $thumbnail[0] : '';
+	
+			if ( $latitude && $longitude ) {
+				$result[] = array(
+					// post specific data
+					'id' => $id,
+					'title' => $title,
+					'excerpt' => $excerpt,
+					'latitude' => $latitude,
+					'longitude' => $longitude,
+					'link' => $link,
+					'thumbnail_url' => $thumbnail_url,
+					// plugin settings
+					'map_pos_latitude' => $map_pos_latitude,
+					'map_pos_longitude' => $map_pos_longitude,
+					'map_zoom' => $map_zoom,
+				);
+			}
+		}
+		wp_send_json( $result );
+		wp_die();
+	}
+}
---
+++ public/class-lensmark-submission_form.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+ * Contains all submission form functionalities.
+ *
+ * @link       https://wbth.m-clement.ch
+ * @since      1.0.0
+ *
+ * @package    Lensmark
+ * @subpackage Lensmark/public
+ */
+
+class Lensmark_Submission_Form {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $lensmark    The ID of this plugin.
+	 */
+	private $lensmark;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $lensmark       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $lensmark, $version ) {
+
+		$this->lensmark = $lensmark;
+		$this->version = $version;
+	}
+
+	/**
+	 * Register the stylesheets
+	 * 
+	 * @since	1.0.0
+	 */
+	public function enqueue_styles() {
+		wp_enqueue_style( 'lensmark-submission_form', plugin_dir_url( __FILE__ ) . 'css/lensmark-submission_form.css', array(), $this->version, 'all' );
+		// enqueue dependencies when the shortcode is on the current page
+	}
+
+    /**
+	 * Add a photo submission page if it not already exists.
+	 * 
+	 * @since    1.0.0
+	 */
+	public static function lensmark_add_submission_form_page() {
+		$post_slug = 'photo_submit';
+		$page_exists = get_page_by_path( $post_slug );
+		if ( $page_exists ) {
+			// Page already exists, don't do anything
+		} else {
+			// Page does not exist
+			$args = array(
+				'post_name' => 'photo_submit',
+				'post_type' => 'page',
+				'page_template' => 'blank', // If exists, select blank template
+				'post_title' => 'Photo submission',
+				'post_status' => 'draft',
+				'post_content' => '[lensmark_submission_form]', // Call shortcode rendering the photo submission form
+			);
+			$post_id = wp_insert_post( $args );
+			if ( ! is_wp_error( $post_id ) ) {
+				//the post is valid
+			} else {
+				//there was an error in the post insertion, 
+				echo $post_id->get_error_message();
+			}
+		}
+	}
+
+	/**
+	 * Remove photo submission page if it already exists.
+	 * 
+	 * @since    1.0.0
+	 */
+	public function lensmark_trash_submission_form_page() {
+		$post_slug = 'photo_submit';
+		$page_exists = get_page_by_path( $post_slug );
+		if ( $page_exists ) {
+			$post_id = $page_exists->ID;
+			wp_trash_post( $post_id );
+		} else {
+			// Page does not exist, don't do anything
+		}
+	}
+
+	/**
+	 * Add new shortcode that displays the submission form.
+	 * 
+	 * @since    1.0.0
+	 */
+	public static function lensmark_add_submission_form_shortcode() {
+		add_shortcode( 'lensmark_submission_form', 'lensmark_shortcode_submission_form_html' );
+		function lensmark_shortcode_submission_form_html( $atts, $content = null ) {
+			/**
+			 * The photopostId must be specified in the url (also for the QR-Code).
+			 * The photopostId is used to assign the submitted photo to the correct photopost
+			 * 
+			 * https://some.site.com/somePage.html?photopostId=[postId]
+			 * */
+			if ( isset( $_GET['photopost_id'] ) ) {
+				$photopost_id = $_GET['photopost_id'];
+				ob_start();
+				?>
+				<h2>Procedure</h2>
+				<ol>
+					<li>Give this website access to your camera app.</li>
+					<li>Place smartphone on bracket.</li>
+					<li>Take photo.</li>
+					<li>Fill out and submit form for approval.</li>
+					<li>Once the photo is approved by the website manager, you will receive a confirmation email.</li>
+				</ol>
+				<h2>Notice</h2>
+				<ul>
+					<li>Take photo with default settings.</li>
+					<li>No flash</li>
+					<li>No wide angle or zoom</li>
+					<li>No filters</li>
+					<li>Do not photograph people</li>
+				</ul>
+				<form id="photo_entry_submission" method="post" action="#" enctype="multipart/form-data">
+					<h2>Submit your photo</h2>
+					<input type="hidden" id="photopost_id" name="photopost_id" value="<?php echo $photopost_id; ?>">
+					<label for="file">Photo:</label>
+					<input type="file" id="photo_entry" name="photo_entry" accept="image" capture="environment" multiple="false">
+					<label for="first-name">First name:</label>
+					<input type="text" id="first-name" name="first-name" required>
+					<label for="last-name">Last name:</label>
+					<input type="text" id="last-name" name="last-name" required>
+					<label for="email">Email:</label>
+					<input type="email" id="email" name="email" required>
+					<span><input type="checkbox" id="terms" name="terms" value="checked" required>
+						<label for="terms">I have read and accept the <a href="" target="_blank">privacy policy</a>.</label>
+					</span>
+					<span><input type="checkbox" id="newsletter" name="newsletter" value="checked">
+						<label for="newsletter">I would like to receive e-mails about the development and results of the photo
+							monitoring project. (Optional)</label>
+					</span>
+					<?php wp_nonce_field( 'photo_entry', 'photo_entry_nonce' ); ?>
+					<input type="submit" id="submit_photo_entry" name="submit_photo_entry" value="Submit">
+				</form>
+				<?php
+				return ob_get_clean();
+			} else {
+				ob_start();
+				?>
+				<h2>Error: Photopost ID does not exist</h2>
+				<p>Please scan the QR-Code again or retype the exact URL of the photopost.</p>
+				<?php
+				return ob_get_clean();
+			}
+		}
+	}
+
+	/**
+	 * Handle photo submission upload and attaching it to the photopost.
+	 * 
+	 * @since    1.0.0
+	 */
+	function lensmark_submit_entry() {
+		// Check that the nonce is valid.
+		if (
+			isset( $_POST['photo_entry_nonce'], $_POST['photopost_id'] )
+			&& wp_verify_nonce( $_POST['photo_entry_nonce'], 'photo_entry' )
+		) {
+			// These files need to be included as dependencies when on the front end.
+			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+			require_once( ABSPATH . 'wp-admin/includes/file.php' );
+			require_once( ABSPATH . 'wp-admin/includes/media.php' );
+
+			// Let WordPress handle the upload.
+			// Remember, 'photo_entry' is the name of our file input in our form above.
+			$attachment_id = media_handle_upload( 'photo_entry', $_POST['photopost_id'] );
+
+			if ( is_wp_error( $attachment_id ) ) {
+				// There was an error uploading the image.
+			} else {
+				// The image was uploaded successfully!
+			}
+
+		} else {
+			// The security check failed, maybe show the user an error.
+		}
+	}
+
+}
